7c23d845cf937adac7208a1fb20e1344bc0a08a2
RitzD <u15075754@tuks.co.za>
2016-08-11 17:34:20 +0200

    Added Main File
    Created the main file with different functions to be edited.

---
+++ Main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+
+void function1(int num1, int num2);
+void function2(int num1, int num2);
+void function3(int num1, int num2);
+void function4(int num1, int num2);
+void function5(int num1, int num2);
+void function6();
+void function7(int num1, int num2);
+void function8(int num1, int num2);
+
+
+int main()
+{
+	int num1 = 93;
+	int num2 = 46;
+	int num3 = 38;
+	int num4 = 24;
+
+	cout << "Mentorship Demo" << endl;
+
+	function1(num1, num2);
+	function2(num2, num4);
+	function3(num3, num1);
+	function4(num2, num1);
+	function5(num2, num4);
+	function6();
+	function7(num2, num4);
+	function8(num1, num3);
+
+	return 0;
+}
+
+
+void function1(int num1, int num2) {
+	//add two numbers and display the result
+
+	cout << "Edited by : " << endl;
+}
+
+void function2(int num1, int num2) {
+	//subtract two numbers and display the result
+
+	cout << "Edited by : " << endl;
+}
+
+void function3(int num1, int num2) {
+	//multiply two numbers and display the result
+
+	cout << "Edited by : " << endl;
+}
+
+void function4(int num1, int num2) {
+	//divide the first number by the second and display the result
+
+	cout << "Edited by : " << endl;
+}
+
+void function5(int num1, int num2) {
+	//divide the first number by the second and display the remainder
+
+	cout << "Edited by : " << endl;
+}
+
+void function6() {
+	//ask the user for a string and display it
+
+	cout << "Edited by : " << endl;
+}
+
+void function7(int num1, int num2) {
+	//find the max of the two numbers and display it
+
+	cout << "Edited by : " << endl;
+}
+
+void function8(int num1, int num2) {
+	//find the min of the two numbers and display it
+
+	cout << "Edited by : " << endl;
+}
+
